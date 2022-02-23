f7cab63ff09cf1b63db9f9bdb682e56a23ec4109
tasnimb <60580746+tasnimb@users.noreply.github.com>
2022-02-23 16:49:58 +0000

    cucumber ext

--- src/test/java/com/purple/cardealership/CarDealership/CarDealershipTest.java
+++ src/test/java/com/purple/cardealership/CarDealership/CarDealershipTest.java
@@ -1,4 +1,22 @@
-@Cucumber
-public class CarDealershipTest{
+package com.purple.cardealership.CarDealership;
+
+import io.cucumber.java.en.When;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/com/purple/cardealership/CarDealership")
+public class CarDealershipTest {
+
+    @When("^the user calls the create endpoint")
+    public void the_user_calls_create() throws Throwable{
+
+        WireMockServer wireMockServer = new WireMockServer(options().dynamicPort());
+
+    }
+
 
 }
