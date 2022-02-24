2c5dc80989a9d62894f02b8cf5ba510c24f8ba8e
tasnimb <60580746+tasnimb@users.noreply.github.com>
2022-02-24 12:45:09 +0000

    create tests created

--- src/test/java/com/purple/cardealership/CarDealership/CarDealership.feature
+++ src/test/java/com/purple/cardealership/CarDealership/CarDealership.feature
@@ -1,6 +1,22 @@
 Feature: Testing car dealership
   Scenario: Creating a car with valid data
-    When the body contains valid data
+    When the body contains "valid" data
     And the user calls the create endpoint
     Then the user should get a response status code of 201
-    And a message of "The car was created"
+    And a "message" of "The car was created"
+
+  Scenario: Creating a car with invalid data
+    When the body contains "invalid" data
+    And the user calls the create endpoint
+    Then the user should get a response status code of 400
+    And a "error" of "Missing/invalid data in post body"
+    And a "internal-error-code" of "CUSTOM_ERROR_BAD_REQUEST"
+
+  Scenario: Reading a car with no parameters
+    And the user calls the get endpoint
+    Then the user should get a response status code of 200
+    And a "message" of "Successfully fetched cars"
+    And a results of cars
+#    And we get result of a car
+
+
--- src/test/java/com/purple/cardealership/CarDealership/CucumberSpringConfiguration.java
+++ src/test/java/com/purple/cardealership/CarDealership/CucumberSpringConfiguration.java
@@ -11,52 +11,73 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
-import wiremock.org.apache.http.HttpEntity;
 import wiremock.org.apache.http.HttpResponse;
+import wiremock.org.apache.http.client.methods.HttpGet;
 import wiremock.org.apache.http.client.methods.HttpPost;
 import wiremock.org.apache.http.entity.StringEntity;
 import wiremock.org.apache.http.impl.client.CloseableHttpClient;
 import wiremock.org.apache.http.impl.client.HttpClients;
 import wiremock.org.apache.http.util.EntityUtils;
 
+import java.util.Objects;
+
 @CucumberContextConfiguration
 @SpringBootTest
 public class CucumberSpringConfiguration {
     HttpResponse response;
     String jsonString = "{}";
+    String body = "";
+    CloseableHttpClient httpClient = HttpClients.createDefault();
 
-    @When("^the body contains valid data")
-    public void the_user_enters_valid_data() throws Throwable {
-        jsonString = "{\"brand\":\"asd\",\"model\":\"asd2\",\"age\":\"123\",\"mileage\":\"123456\",\"engineSize\":\"1.4\"}";
+    @When("the body contains {string} data")
+    public void the_user_enters_invalid_or_valid_data(String status){
+        if(Objects.equals(status, "valid")){
+            jsonString = "{\"brand\":\"asd\",\"model\":\"asd2\",\"age\":\"123\",\"mileage\":\"123456\",\"engineSize\":\"1.4\"}";
+        } else {
+            jsonString = "{\"model\":\"asd2\",\"age\":\"123\",\"mileage\":\"123456\",\"engineSize\":\"1.4\"}";
+        }
+
     }
 
-    @And("^the user calls the create endpoint")
+    @And("the user calls the create endpoint")
     public void the_user_calls_create() throws Throwable {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
 
         HttpPost request = new HttpPost("http://localhost:8080/car/create");
         StringEntity entity = new StringEntity(jsonString);
         request.addHeader("content-type", "application/json");
         request.setEntity(entity);
         response = httpClient.execute(request);
-        // assertEquals("asd", EntityUtils.toString(response.getEntity(), "UTF-8"));
+        body = EntityUtils.toString(response.getEntity(), "UTF-8");
     }
 
-    @Then("^the user should get a response status code of 201")
-    public void the_user_gets_a_response() throws Throwable {
-        // throw new PendingException();
-        assertEquals("201", String.valueOf(response.getStatusLine().getStatusCode()));
+    @Then("the user should get a response status code of {int}")
+    public void the_user_gets_a_response_code(int code){
+        assertEquals(code, response.getStatusLine().getStatusCode());
     }
 
-    @And("^a message of \"The car was created\"")
-    public void the_user_gets_a_success_message() throws Throwable {
-        // throw new PendingException();
-        // EntityUtils.toString(response.getEntity(), "UTF-8")
-        HttpEntity entity = response.getEntity();
-
-        String body = EntityUtils.toString(entity, "UTF-8");
+    @And("a {string} of {string}")
+    public void the_user_gets_a_body_containing_key_value_pair(String key, String value){
         JsonObject jsonBody = JsonParser.parseString(body).getAsJsonObject();
-        assertEquals("The car was created", jsonBody.get("message").getAsString());
+        assertEquals(value, jsonBody.get(key).getAsString());
     }
+
+    @And("a results of cars")
+    public void the_user_gets_results_of_cars(){
+        JsonObject jsonBody = JsonParser.parseString(body).getAsJsonObject();
+        String result = jsonBody.get("results").getAsJsonArray().get(0).getAsJsonObject().get("id").getAsString();
+        assertEquals("1",result);
+    }
+
+
+    @And("the user calls the get endpoint")
+    public void the_user_calls_get() throws Throwable {
+        HttpGet request =  new HttpGet("http://localhost:8080/car/read");
+        response = httpClient.execute(request);
+        body = EntityUtils.toString(response.getEntity(), "UTF-8");
+
+    }
+
+
+
 
 }
